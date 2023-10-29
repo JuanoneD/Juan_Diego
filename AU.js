bf44a00bb6c2c573c0099b9ace0846dd95e21066
Juan Diego <juandiego.jd42@Gmail.com>
2023-10-29 18:14:44 -0300

    pi

---
+++ AU.js
@@ -0,0 +1,31 @@
+var a;
+a=0;
+
+document.getElementById("java").innerHTML = "Odeio ficar triste";
+function chorar(){
+    document.getElementById("java").innerHTML="CHORAR";
+};
+function lagrimas(){
+    document.getElementById("java").innerHTML= 23+11;
+};
+function aler(){
+    window.alert("login sucesso");
+    a=1;
+    alert(a);
+};
+function colo(){
+    console.log(5+33);
+    if (a==1){
+        alert("voce está logado")
+    }else{
+        alert("voce não está logado")
+    }
+};
+function imp(){
+    window.print();
+};
+function limp(){
+    document.getElementById("tris").innerHTML="estou tristeza";
+    document.getElementById("java").innerHTML="";
+    a=0;
+};
