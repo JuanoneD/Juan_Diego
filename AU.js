552186a8bbb3d2c91d4ec2d971126c4bbdd4dec1
Juan Diego <juandiego.jd42@Gmail.com>
2023-11-12 17:56:17 -0300

    a

--- AU.js
+++ AU.js
@@ -1,31 +1,22 @@
-var a;
-a=0;
+function logar() {
+    a = 1;
+    alert(a); // Usando 'alert' para exibir o valor atual de 'a'
+    atualizarMenu(); // Chama a função para atualizar o menu
+}
 
-document.getElementById("java").innerHTML = "Odeio ficar triste";
-function chorar(){
-    document.getElementById("java").innerHTML="CHORAR";
-};
-function lagrimas(){
-    document.getElementById("java").innerHTML= 23+11;
-};
-function aler(){
-    window.alert("login sucesso");
-    a=1;
-    alert(a);
-};
-function colo(){
-    console.log(5+33);
-    if (a==1){
-        alert("voce está logado")
-    }else{
-        alert("voce não está logado")
+function deslogar() {
+    a = 0;
+    alert(a); // Usando 'alert' para exibir o valor atual de 'a'
+    atualizarMenu(); // Chama a função para atualizar o menu
+}
+
+function atualizarMenu() {
+    alert(a); // Usando 'alert' para exibir o valor atual de 'a'
+    if (a == 1) {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("logmenu").style.display = "block";
+    } else {
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("logmenu").style.display = "none";
     }
-};
-function imp(){
-    window.print();
-};
-function limp(){
-    document.getElementById("tris").innerHTML="estou tristeza";
-    document.getElementById("java").innerHTML="";
-    a=0;
-};
+}
