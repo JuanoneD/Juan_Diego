bbe7fd3422718d68d89e1c4d996be2de58b45826
Juan Diego <juandiego.jd42@Gmail.com>
2023-11-12 20:47:30 -0300

    A

--- AU.js
+++ AU.js
@@ -1,17 +1,14 @@
 function logar() {
     a = 1;
-    alert(a); // Usando 'alert' para exibir o valor atual de 'a'
     atualizarMenu(); // Chama a função para atualizar o menu
 }
 
 function deslogar() {
     a = 0;
-    alert(a); // Usando 'alert' para exibir o valor atual de 'a'
     atualizarMenu(); // Chama a função para atualizar o menu
 }
 
 function atualizarMenu() {
-    alert(a); // Usando 'alert' para exibir o valor atual de 'a'
     if (a == 1) {
         document.getElementById("menu").style.display = "none";
         document.getElementById("logmenu").style.display = "block";
@@ -20,3 +17,8 @@ function atualizarMenu() {
         document.getElementById("logmenu").style.display = "none";
     }
 }
+
+// function prer_tab(){
+//     document.getElementById("tabela1");
+//     document.createElement("tr");
+// }
