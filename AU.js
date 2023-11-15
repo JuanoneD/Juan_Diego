a6d11cb16052f7c9a2c49206d5ca54e799aa0cbe
Juan Diego <juandiego.jd42@Gmail.com>
2023-11-15 16:57:40 -0300

    a

--- AU.js
+++ AU.js
@@ -1,12 +1,12 @@
 
   function logar() {
     a = 1;
-    atualizarMenu(); // Chama a função para atualizar o menu
+    atualizarMenu(); 
 }
 
 function deslogar() {
     a = 0;
-    atualizarMenu(); // Chama a função para atualizar o menu
+    atualizarMenu(); 
 }
 
 function atualizarMenu() {
@@ -20,25 +20,23 @@ function atualizarMenu() {
 }
 
   
-// Suponha que você tenha um array de objetos representando dados
+
 const dados = [
     { nome: 'Jão Carlos', email: 'jãocarlos@gmail.com', telefone: '41 9991-1233', endereco: 'Rua da Terra Roxa' },
     { nome: 'Jão Carlos', email: 'jãocarlos@gmail.com', telefone: '41 9991-1233', endereco: 'Rua da Terra Roxa' },
     { nome: 'Jão Carlos', email: 'jãocarlos@gmail.com', telefone: '41 9991-1233', endereco: 'Rua da Terra Roxa' },
-    // Adicione mais objetos conforme necessário
   ];
   
-  // Função para preencher a tabela
   function preencherTabela() {
     const tabela = document.getElementById('minhaTabela');
     const corpoTabela = tabela.getElementsByTagName('tbody')[0];
   
-    // Percorre os dados e cria uma linha para cada item
+
     for (let i = 0; i < dados.length; i++) {
       const linha = document.createElement('tr');
       const dado = dados[i];
   
-      // Cria células para cada propriedade do objeto
+
       const celulaNome = document.createElement('td');
       celulaNome.textContent = dado.nome;
       const celulaEmail = document.createElement('td');
@@ -49,7 +47,7 @@ const dados = [
       celulaEndereco.textContent = dado.endereco;
       const celulaAcoes = document.createElement('td');
       
-      // Criação dos links de ação
+
       const linkEditar = document.createElement('a');
       linkEditar.href = '#';
       const imgEditar = document.createElement('img');
@@ -64,18 +62,16 @@ const dados = [
       imgExcluir.height = '20px';
       linkExcluir.appendChild(imgExcluir);
   
-      // Adiciona os links de ação na célula de ações
+
       celulaAcoes.appendChild(linkEditar);
       celulaAcoes.appendChild(linkExcluir);
-  
-      // Adiciona as células na linha
+
       linha.appendChild(celulaNome);
       linha.appendChild(celulaEmail);
       linha.appendChild(celulaTelefone);
       linha.appendChild(celulaEndereco);
       linha.appendChild(celulaAcoes);
   
-      // Adiciona a linha ao corpo da tabela
       corpoTabela.appendChild(linha);
     }
   }
